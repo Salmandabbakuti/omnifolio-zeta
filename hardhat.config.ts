a10ccf489068f134c23833b786fd733cf7309ac5
Salman Dabbakuti <dabbakutisalman@gmail.com>
2024-07-13 22:08:22 +0530

    setup: zeta template

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,13 @@
-import { HardhatUserConfig, vars } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
-const accounts = vars.has("PRIVATE_KEY") ? [vars.get("PRIVATE_KEY")] : [];
+import "@zetachain/toolkit/tasks";
+import { getHardhatConfigNetworks } from "@zetachain/networks";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   defaultNetwork: "localhost",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    },
-    amoy: {
-      url: "https://rpc-amoy.polygon.technology/",
-      accounts
-    }
+    ...getHardhatConfigNetworks()
   }
 };
 
